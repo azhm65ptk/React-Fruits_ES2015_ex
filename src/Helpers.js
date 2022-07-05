aa1aebc8371a216c5ccc5dec9ac836023ec38846
Aung Myint <Austin@AungZinyintsMBP.lan>
2022-07-05 01:44:47 -0400

    react ES2015 ex

---
+++ src/Helpers.js
@@ -0,0 +1,19 @@
+function Choice(items){
+    let idx=Math.floor(Math.random()* items.length);
+    return (
+        items[idx]
+    )
+
+    
+}
+
+function Remove(items, item){
+    for(let i=0; i< items.length; i++){
+        if(items[i]===item){
+            return[...items.slice(0,i),...items.slice(i+1)]
+        }
+    }
+}
+
+export {Choice,Remove}
+
